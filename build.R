3b0b5c6ea2cda8f5daf7841e6cdc4800f589e819
Andrew Heiss <andrew@andrewheiss.com>
2025-09-26 00:21:15 -0400

    Add stuff for uploading

--- build.R
+++ build.R
@@ -3,3 +3,14 @@ quarto::quarto_render(".", output_format = "all")
 
 # README
 quarto::quarto_render("README.qmd")
+
+# Upload site
+base_url <- "https://stats.andrewheiss.com/snoopy-spring/"
+
+upload_out <- processx::run("./deploy.sh")
+
+if (upload_out$status == 0) {
+  cli::cli_alert_success(paste0("Website uploaded to ", base_url))
+} else {
+  cli::cli_alert_danger(paste0("Something went wrong! ", upload_out$stderr))
+}
